e5cb4a29281ac09ee633fd5d2295a33e91823a42
Aleksander Jedynak <aleksander.jedynak@softax.pl>
2023-05-12 17:11:42 +0200

    first commit

---
+++ app/page.js
@@ -0,0 +1,9 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col text-2xl items-center justify-between p-24">
+      Aleks
+    </main>
+  )
+}
---
+++ next.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+      // # gdy chcemy wygenerowac pliki statyczne
+      // output: 'export',
+}
+
+module.exports = nextConfig
